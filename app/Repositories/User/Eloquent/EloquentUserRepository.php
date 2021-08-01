d71666f23877af6922724f86aa89415e2683d42b
ahmed kandil <ahmedkandil2015@hotmail.com>
2021-08-01 10:38:32 +0200

    create user repoitory

---
+++ app/Repositories/User/Eloquent/EloquentUserRepository.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Repositories\User\Eloquent;
+
+use App\Repositories\EloquentBaseRepository;
+use App\Repositories\User\UserRepository;
+
+class EloquentUserRepository extends EloquentBaseRepository implements UserRepository
+{
+
+}
