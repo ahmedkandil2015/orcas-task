f326e7c902a06c74e16e9549c08fa7190053b6cb
ahmed kandil <ahmedkandil2015@hotmail.com>
2021-08-01 10:59:02 +0200

    create list and search apis routs

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\V1\User\UserController;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::group(['prefix' => 'v1', 'namespace' => 'API/V1'], function (Router $route) {
+
+    $route->group([], function (Router $route) {
+
+            $route->group(['prefix' => 'users'], function (Router $route) {
+                    $route->get('list', [UserController::class, 'list']);
+                    $route->put('search', [UserController::class, 'search']);
+            });
+    });
 });
