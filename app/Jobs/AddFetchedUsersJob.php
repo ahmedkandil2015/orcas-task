18be6b3284403a02a776d612aaed50a27bc89fda
ahmed kandil <ahmedkandil2015@hotmail.com>
2021-08-01 09:55:55 +0200

    insert new users as a bulk in to users table

--- app/Jobs/AddFetchedUsersJob.php
+++ app/Jobs/AddFetchedUsersJob.php
@@ -8,19 +8,22 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
 
 class AddFetchedUsersJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    private $users,$schema;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($users , $schema)
     {
-        //
+        $this->users=$users;
+        $this->schema=$schema;
     }
 
     /**
@@ -29,7 +32,18 @@ class AddFetchedUsersJob implements ShouldQueue
      * @return void
      */
     public function handle()
-    {
+    {  
+        $users = (collect($this->users))->map(function($user){
+
+            return [
+                "email"=>$user[$this->schema['email']],
+                "firstName"=>$user[$this->schema['firstName']],
+                "lastName"=>$user[$this->schema['lastName']],
+                "avatar"=>$user[$this->schema['avatar']]
+            ];
+        })->toArray();
+        DB::table('users')->insert($users); // Query Builder approach
+
         //
     }
 }
