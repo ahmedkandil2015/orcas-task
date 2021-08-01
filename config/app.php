46fa25f349b9cb403643e2259034c0c6f481bd43
ahmed kandil <ahmedkandil2015@hotmail.com>
2021-08-01 09:05:20 +0200

    create user sources in config file

--- config/app.php
+++ config/app.php
@@ -231,4 +231,8 @@ return [
 
     ],
 
+    'user-sources'=>explode(',', env('USERS_SOURCE',"https://60e1b5fc5a5596001730f1d6.mockapi.io/api/v1/users/users_1,https://60e1b5fc5a5596001730f1d6.mockapi.io/api/v1/users/user_2"))
+    
+       
+
 ];
